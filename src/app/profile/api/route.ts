4027281a7795fe920808bbf665cd33a66e81298d
Abhishek Aman <abhiofficial.1599@gmail.com>
2024-07-12 01:30:36 +0530

    Till Lecture 42

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,14 +1,23 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 
 export async function GET(request: NextRequest){
     const requestHeader = new Headers(request.headers);
     console.log(requestHeader.get("Authorization"));
     const headerList = headers();
     console.log(headerList.get("Authorization"));
+
+    //Cookie Approach 2
+    cookies().set("resultsPerPage","20");
+    console.log(cookies().get("resultsPerPage"));
+
+    //Cookie Approach 1
+    const theme = request.cookies.get("theme");
+    console.log(theme);
     return new Response("<h1>Profile API Data</h1>",{
         headers:{
-            "Content-Type":"text/html"
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark",
         }
     });
 }
